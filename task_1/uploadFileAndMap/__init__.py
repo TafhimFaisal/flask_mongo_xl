1730baccb538705406f2a80ba1c19fa595d17a03
TafhimFaisal <tropical10tafhim@gmail.com>
2023-11-02 19:51:59 +0600

    env

--- task_1/uploadFileAndMap/__init__.py
+++ task_1/uploadFileAndMap/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from pymongo import MongoClient
+from dotenv import dotenv_values
 
+config = dotenv_values()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-client = MongoClient('mongodb://localhost:27017/')
-db = client['product_data']
+app.config['SECRET_KEY'] = config['SECRET_KEY']
+client = MongoClient(config['MONGO_DB'])
+db = client[config['DB_NAME']]
 
 from uploadFileAndMap import controller
