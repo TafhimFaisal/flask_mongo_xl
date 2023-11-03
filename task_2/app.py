9d114487054e2713bdfa83037476317d785d270b
TafhimFaisal <tropical10tafhim@gmail.com>
2023-11-03 21:39:49 +0600

    xero intigrations

--- task_2/app.py
+++ task_2/app.py
@@ -1,86 +1,77 @@
-# -*- coding: utf-8 -*-
 import os
+from pathlib import Path
+from random import randint
 from functools import wraps
-from io import BytesIO
 from logging.config import dictConfig
 
-from flask import Flask, url_for, render_template, session, redirect, json, send_file
+from flask import Flask, url_for, render_template, session, redirect, json,flash
 from flask_oauthlib.contrib.client import OAuth, OAuth2Application
 from flask_session import Session
-from xero_python.accounting import AccountingApi, ContactPerson, Contact, Contacts
-from xero_python.api_client import ApiClient, serialize
+from xero_python.accounting import AccountingApi
+from xero_python.api_client import ApiClient
 from xero_python.api_client.configuration import Configuration
 from xero_python.api_client.oauth2 import OAuth2Token
 from xero_python.exceptions import AccountingBadRequestException
 from xero_python.identity import IdentityApi
-from xero_python.utils import getvalue
 
 import logging_settings
 from utils import jsonify, serialize_model
 from dotenv import dotenv_values
 
 config = dotenv_values()
-
 dictConfig(logging_settings.default_settings)
 
-# configure main flask application
 app = Flask(__name__)
 app.secret_key = config['SECRET_KEY']
-app.session_cookie_name = 'xero_flask_cookie'
+app.session_cookie_name = config['SESSION_COOKIE_NAME']
 
 app.config.from_object("default_settings")
 app.config.from_pyfile("config.py", silent=True)
 
+
 if app.config["ENV"] != "production":
-    # allow oauth2 loop to run over http (used for local testing only)
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-# configure persistent session cache
 Session(app)
-
-# configure flask-oauthlib application
-# TODO fetch config from https://identity.xero.com/.well-known/openid-configuration #1
 oauth = OAuth(app)
 xero = oauth.remote_app(
     name="xero",
     version="2",
-    client_id=config["CLIENT_ID"],
-    client_secret=config["CLIENT_SECRET"],
+    client_id=app.config["CLIENT_ID"],
+    client_secret=app.config["CLIENT_SECRET"],
     endpoint_url="https://api.xero.com/",
     authorization_url="https://login.xero.com/identity/connect/authorize",
     access_token_url="https://identity.xero.com/connect/token",
     refresh_token_url="https://identity.xero.com/connect/token",
     scope="offline_access openid profile email accounting.transactions "
-    "accounting.reports.read accounting.journals.read accounting.settings "
-    "accounting.contacts accounting.attachments assets projects",
-)  # type: OAuth2Application
+    "accounting.transactions.read accounting.reports.read "
+    "accounting.journals.read accounting.settings accounting.settings.read "
+    "accounting.contacts accounting.contacts.read accounting.attachments "
+    "accounting.attachments.read assets projects "
+    "files "
+    "payroll.employees payroll.payruns payroll.payslip payroll.timesheets payroll.settings",
+) 
 
-
-# configure xero-python sdk client
 api_client = ApiClient(
     Configuration(
-        debug=config["DEBUG"],
+        debug=app.config["DEBUG"],
         oauth2_token=OAuth2Token(
-            client_id=config["CLIENT_ID"], client_secret=config["CLIENT_SECRET"]
+            client_id=app.config["CLIENT_ID"], client_secret=app.config["CLIENT_SECRET"]
         ),
     ),
     pool_threads=1,
 )
 
-
-# configure token persistence and exchange point between flask-oauthlib and xero-python
 @xero.tokengetter
 @api_client.oauth2_token_getter
 def obtain_xero_oauth2_token():
     return session.get("token")
-
 
 @xero.tokensaver
 @api_client.oauth2_token_saver
 def store_xero_oauth2_token(token):
     session["token"] = token
     session.modified = True
-
 
 def xero_token_required(function):
     @wraps(function)
@@ -93,193 +84,26 @@ def xero_token_required(function):
 
     return decorator
 
+def attachment_image():
+    return Path(__file__).resolve().parent.joinpath("helo-heros.jpg")
 
-@app.route("/")
-def index():
-    xero_access = dict(obtain_xero_oauth2_token() or {})
-    return render_template(
-        "code.html",
-        title="Home | oauth token",
-        code=json.dumps(xero_access, sort_keys=True, indent=4),
-    )
+def get_code_snippet(endpoint,action):
+    s = open("app.py").read()
+    startstr = "["+ endpoint +":"+ action +"]"
+    endstr = "#[/"+ endpoint +":"+ action +"]"
+    start = s.find(startstr) + len(startstr)
+    end = s.find(endstr)
+    substring = s[start:end]
+    return substring
 
+def get_random_num():
+    return str(randint(0, 10000))
 
-@app.route("/tenants")
-@xero_token_required
-def tenants():
+def get_connection_id():
     identity_api = IdentityApi(api_client)
-    accounting_api = AccountingApi(api_client)
-
-    available_tenants = []
     for connection in identity_api.get_connections():
-        tenant = serialize(connection)
         if connection.tenant_type == "ORGANISATION":
-            organisations = accounting_api.get_organisations(
-                xero_tenant_id=connection.tenant_id
-            )
-            tenant["organisations"] = serialize(organisations)
-
-        available_tenants.append(tenant)
-
-    return render_template(
-        "code.html",
-        title="Xero Tenants",
-        code=json.dumps(available_tenants, sort_keys=True, indent=4),
-    )
-
-
-@app.route("/create-contact-person")
-@xero_token_required
-def create_contact_person():
-    xero_tenant_id = get_xero_tenant_id()
-    accounting_api = AccountingApi(api_client)
-
-    contact_person = ContactPerson(
-        first_name="John",
-        last_name="Smith",
-        email_address="john.smith@24locks.com",
-        include_in_emails=True,
-    )
-    contact = Contact(
-        name="FooBar",
-        first_name="Foo",
-        last_name="Bar",
-        email_address="ben.bowden@24locks.com",
-        contact_persons=[contact_person],
-    )
-    contacts = Contacts(contacts=[contact])
-    try:
-        created_contacts = accounting_api.create_contacts(
-            xero_tenant_id, contacts=contacts
-        )  # type: Contacts
-    except AccountingBadRequestException as exception:
-        sub_title = "Error: " + exception.reason
-        code = jsonify(exception.error_data)
-    else:
-        sub_title = "Contact {} created.".format(
-            getvalue(created_contacts, "contacts.0.name", "")
-        )
-        code = serialize_model(created_contacts)
-
-    return render_template(
-        "code.html", title="Create Contacts", code=code, sub_title=sub_title
-    )
-
-
-@app.route("/create-multiple-contacts")
-@xero_token_required
-def create_multiple_contacts():
-    xero_tenant_id = get_xero_tenant_id()
-    accounting_api = AccountingApi(api_client)
-
-    contact = Contact(
-        name="George Jetson",
-        first_name="George",
-        last_name="Jetson",
-        email_address="george.jetson@aol.com",
-    )
-    # Add the same contact twice - the first one will succeed, but the
-    # second contact will fail with a validation error which we'll show.
-    contacts = Contacts(contacts=[contact, contact])
-    try:
-        created_contacts = accounting_api.create_contacts(
-            xero_tenant_id, contacts=contacts, summarize_errors=False
-        )  # type: Contacts
-    except AccountingBadRequestException as exception:
-        sub_title = "Error: " + exception.reason
-        result_list = None
-        code = jsonify(exception.error_data)
-    else:
-        sub_title = ""
-        result_list = []
-        for contact in created_contacts.contacts:
-            if contact.has_validation_errors:
-                error = getvalue(contact.validation_errors, "0.message", "")
-                result_list.append("Error: {}".format(error))
-            else:
-                result_list.append("Contact {} created.".format(contact.name))
-
-        code = serialize_model(created_contacts)
-
-    return render_template(
-        "code.html",
-        title="Create Multiple Contacts",
-        code=code,
-        result_list=result_list,
-        sub_title=sub_title,
-    )
-
-
-@app.route("/invoices")
-@xero_token_required
-def get_invoices():
-    xero_tenant_id = get_xero_tenant_id()
-    accounting_api = AccountingApi(api_client)
-
-    invoices = accounting_api.get_invoices(
-        xero_tenant_id, statuses=["DRAFT", "SUBMITTED"]
-    )
-    code = serialize_model(invoices)
-    sub_title = "Total invoices found: {}".format(len(invoices.invoices))
-
-    return render_template(
-        "code.html", title="Invoices", code=code, sub_title=sub_title
-    )
-
-
-@app.route("/login")
-def login():
-    redirect_url = url_for("oauth_callback", _external=True)
-    response = xero.authorize(callback_uri=redirect_url)
-    return response
-
-
-@app.route("/callback")
-def oauth_callback():
-    try:
-        response = xero.authorized_response()
-    except Exception as e:
-        print(e)
-        raise
-    # todo validate state value
-    if response is None or response.get("access_token") is None:
-        return "Access denied: response=%s" % response
-    store_xero_oauth2_token(response)
-    return redirect(url_for("index", _external=True))
-
-
-@app.route("/logout")
-def logout():
-    store_xero_oauth2_token(None)
-    return redirect(url_for("index", _external=True))
-
-
-@app.route("/export-token")
-@xero_token_required
-def export_token():
-    token = obtain_xero_oauth2_token()
-    buffer = BytesIO("token={!r}".format(token).encode("utf-8"))
-    buffer.seek(0)
-    return send_file(
-        buffer,
-        mimetype="x.python",
-        as_attachment=True,
-        attachment_filename="oauth2_token.py",
-    )
-
-
-@app.route("/refresh-token")
-@xero_token_required
-def refresh_token():
-    xero_token = obtain_xero_oauth2_token()
-    new_token = api_client.refresh_oauth2_token()
-    return render_template(
-        "code.html",
-        title="Xero OAuth2 token",
-        code=jsonify({"Old Token": xero_token, "New token": new_token}),
-        sub_title="token refreshed",
-    )
-
+            return connection.id
 
 def get_xero_tenant_id():
     token = obtain_xero_oauth2_token()
@@ -291,6 +115,59 @@ def get_xero_tenant_id():
         if connection.tenant_type == "ORGANISATION":
             return connection.tenant_id
 
+@app.route("/")
+def index():
+    xero_access = dict(obtain_xero_oauth2_token() or {})
+    return render_template(
+        "index.html",
+        title="Home",
+        code=json.dumps(xero_access, sort_keys=True, indent=4),
+    )
 
-if __name__ == '__main__':
+@app.route("/accounts")
+@xero_token_required
+def accounts():
+    data = None
+    try:
+        xero_tenant_id = get_xero_tenant_id()
+        accounting_api = AccountingApi(api_client)
+        order = 'Name ASC'
+        data = accounting_api.get_accounts(xero_tenant_id, order)
+    except AccountingBadRequestException as exception:
+        flash(exception.reason, 'error')
+
+    return render_template("accounts.html",data=data)
+
+@app.route("/login")
+def login():
+    redirect_url = url_for("oauth_callback", _external=True)
+    session["state"] = app.config["STATE"]
+    response = xero.authorize(callback_uri=redirect_url, state=session["state"])
+    return response
+
+@app.route("/logout")
+def logout():
+    store_xero_oauth2_token(None)
+    return redirect(url_for("index", _external=True))
+
+@app.route("/callback")
+def oauth_callback():
+    response = xero.authorized_response()
+    if response is None or response.get("access_token") is None:
+        return "Access denied: response=%s" % response
+    store_xero_oauth2_token(response)
+    return redirect(url_for("index", _external=True))
+
+
+@app.route("/disconnect")
+def disconnect():
+    connection_id = get_connection_id()
+    identity_api = IdentityApi(api_client)
+    identity_api.delete_connection(
+        id=connection_id
+    )
+
+    return redirect(url_for("index", _external=True))
+
+if __name__ == "__main__":
     app.run(host='localhost', port=5000)
