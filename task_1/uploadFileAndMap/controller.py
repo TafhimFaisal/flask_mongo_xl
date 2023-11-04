967d803dd50e0199a8c61527872c339fde08ed87
TafhimFaisal <tropical10tafhim@gmail.com>
2023-11-04 19:56:37 +0600

    editable excel

--- task_1/uploadFileAndMap/controller.py
+++ task_1/uploadFileAndMap/controller.py
@@ -5,16 +5,45 @@ from uploadFileAndMap import app,db
 from bson import ObjectId
 from pathlib import Path
 
-@app.route("/update/mapping",methods=['POST'])
-def update_mapping():
+def get_all_error_messages(form):
+    error_messages = []
+    for field, errors in form.errors.items():
+        for error in errors:
+            error_messages.append(f"{form[field].label.text}: {error}")
+    return ', '.join(error_messages)
+
+def get_data(type = None):
+    data = None
+    if 'product_collection' not in db.list_collection_names():
+        db.create_collection('product_collection')
+    else:
+        collection = db['product_collection']
+        result = collection.find({'map':type})
+        data = result
+
+    return data
+
+@app.route("/upload_mapped_data",methods=['POST'])
+def upload_mapped_data():
     collection = db['product_collection']
-    form_data = request.form.to_dict()
-    collection.update_one({"_id": ObjectId(form_data['object_id'])}, {"$set": {'map':form_data['map']}})
-    flash('Record updated successfully.', 'success')
+    mapform = MapUploadFileForm()
+    if mapform.validate_on_submit(): 
+        data = [ field.data for field in mapform.map if field.data['map'] != "None"]
+        collection.insert_many(data)
+        flash('stored successfully.', 'success')
+    elif request.method == 'POST':
+        error_messages = get_all_error_messages(mapform)
+        flash('Form submission failed. Please correct the errors: {}'.format(error_messages), 'danger')
+
     return redirect(url_for('home'))
-    
+
+@app.route("/list/<type>",methods=['GET'])
+def data_list(type):
+    data = get_data(type)
+    return render_template('mapped_data.html',data=data)
+
 @app.route("/",methods=['GET','POST'], endpoint='home')
-def upload_file_and_map():
+def upload_file_and_show():
     form = UploadFileForm()
     mapform = MapUploadFileForm()
     data = None
@@ -24,23 +53,9 @@ def upload_file_and_map():
         uploaded_file = request.files['uploadFileField']
 
         if uploaded_file:
-            path = Path('uploads')
-            if not path.exists():
-                path.mkdir(parents=True, exist_ok=True)
-                
-            file_path = 'uploads/' + uploaded_file.filename
-            uploaded_file.save(file_path)
-            df = pd.read_excel(file_path)
-
-            collection = db['product_collection']
+            df = pd.read_excel(uploaded_file)
             records = df.to_dict('records')
-            collection.insert_many(records)
-
-    if 'product_collection' not in db.list_collection_names():
-        db.create_collection('product_collection')
-    else:
-        collection = db['product_collection']
-        data = collection.find()
+            data = records
 
     return render_template('index.html', 
                 data=data,
